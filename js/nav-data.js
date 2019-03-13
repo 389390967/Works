a32c4443b887114735dadbff916bad50fe38492d
389390968 <389390967@qq.com>
2019-03-13 17:52:36 +0800

    字符串

---
+++ js/nav-data.js
@@ -0,0 +1,108 @@
+var navData = [
+    // {
+    //     "title": "HTML",
+    //     "href": "https://www.1.com/"
+    // },
+
+    {
+        "title": "CSS",
+        "href": "https://www.2.com/",
+        "children": [{
+            "title": "position",
+            "href": "javascript:;",
+            "children": [{
+                "title": "relative",
+                "href": "https://www.21.com/"
+            }, {
+                "title": "absolute",
+                "href": "https://www.22.com/"
+            }]
+        }, {
+            "title": "display",
+            "href": "javascript:;",
+            "children": [{
+                "title": "flex",
+                "href": "https://www.21.com/"
+            }, {
+                "title": "block",
+                "href": "https://www.22.com/"
+            }]
+        }]
+    },
+
+    {
+        "title": "CSS",
+        "href": "https://www.2.com/",
+        "children": [{
+            "title": "position",
+            "href": "javascript:;",
+            "children": [{
+                "title": "relative",
+                "href": "https://www.21.com/"
+            }, {
+                "title": "absolute",
+                "href": "https://www.22.com/"
+            }]
+        }, {
+            "title": "display",
+            "href": "javascript:;",
+            "children": [{
+                "title": "flex",
+                "href": "https://www.21.com/"
+            }, {
+                "title": "block",
+                "href": "https://www.22.com/"
+            }]
+        }]
+    },
+
+    {
+        "title": "CSS",
+        "href": "https://www.2.com/",
+        "children": [{
+            "title": "position",
+            "href": "javascript:;",
+            "children": [{
+                "title": "relative",
+                "href": "https://www.21.com/"
+            }, {
+                "title": "absolute",
+                "href": "https://www.22.com/"
+            }]
+        }, {
+            "title": "display",
+            "href": "javascript:;",
+            "children": [{
+                "title": "flex",
+                "href": "https://www.21.com/"
+            }, {
+                "title": "block",
+                "href": "https://www.22.com/"
+            }]
+        }]
+    },
+
+    // {
+    //     "title": "HTML",
+    //     "href": "https://www.1.com/"
+    // },
+
+    // {
+    //     "title": "HTML",
+    //     "href": "https://www.1.com/"
+    // },
+
+    // {
+    //     "title": "HTML",
+    //     "href": "https://www.1.com/"
+    // },
+
+    // {
+    //     "title": "HTML",
+    //     "href": "https://www.1.com/"
+    // },
+]
+
+function navDataFn() {
+    return navData;
+}
