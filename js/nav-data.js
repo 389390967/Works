61beb4396b3b4be34084bc6d6f6d5d6f4d42468c
389390968 <389390967@qq.com>
2019-03-15 17:50:11 +0800

    改首页

--- js/nav-data.js
+++ js/nav-data.js
@@ -1,108 +1,328 @@
-var navData = [
-    // {
-    //     "title": "HTML",
-    //     "href": "https://www.1.com/"
-    // },
-
+var data = [
+    //HTML
     {
-        "title": "CSS",
-        "href": "https://www.2.com/",
-        "children": [{
-            "title": "position",
-            "href": "javascript:;",
-            "children": [{
-                "title": "relative",
-                "href": "https://www.21.com/"
-            }, {
-                "title": "absolute",
-                "href": "https://www.22.com/"
-            }]
-        }, {
-            "title": "display",
-            "href": "javascript:;",
-            "children": [{
-                "title": "flex",
-                "href": "https://www.21.com/"
-            }, {
-                "title": "block",
-                "href": "https://www.22.com/"
+        "title": "HTML",
+        "link": "javascript:;",
+        "child": [{
+            "title": "input:checkbox",
+            "link": "javascript:;",
+            "child": [{
+                "title": "自定义复选框",
+                "link": "all/demo-html/checkbox/自定义复选框.html"
             }]
         }]
     },
-
+    //CSS
     {
         "title": "CSS",
-        "href": "https://www.2.com/",
-        "children": [{
-            "title": "position",
-            "href": "javascript:;",
-            "children": [{
-                "title": "relative",
-                "href": "https://www.21.com/"
+        "link": "javascript:;",
+        "child": [{
+                "title": "position",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "relative",
+                    "link": "all/demo-css/position/relative.html"
+                }, {
+                    "title": "破坏性、包裹性",
+                    "link": "all/demo-css/position/absolute-1.html"
+                }, {
+                    "title": "脱离文档",
+                    "link": "all/demo-css/position/absolute-2.html"
+                }, {
+                    "title": "超越overflow",
+                    "link": "all/demo-css/position/absolute-3.html"
+                }, {
+                    "title": "去浮动特性",
+                    "link": "all/demo-css/position/absolute-4.html"
+                }, {
+                    "title": "位置跟随特性",
+                    "link": "all/demo-css/position/absolute-5.html"
+                }, {
+                    "title": "不依赖relative的图标定位",
+                    "link": "all/demo-css/position/absolute-6.html"
+                }, {
+                    "title": "不依赖relative的下拉框定位",
+                    "link": "all/demo-css/position/absolute-7.html"
+                }, {
+                    "title": "居中、侧边栏定位",
+                    "link": "all/demo-css/position/absolute-8.html"
+                }, {
+                    "title": "注册页面实例",
+                    "link": "all/demo-css/position/absolute-9.html"
+                }, {
+                    "title": "元素拉伸无固定width height",
+                    "link": "all/demo-css/position/absolute-10.html"
+                }, {
+                    "title": "自适应全屏、一侧固定width另外一侧自适应",
+                    "link": "all/demo-css/position/absolute-11.html"
+                }, {
+                    "title": "高度自适应的九宫格效果",
+                    "link": "all/demo-css/position/absolute-12.html"
+                }, {
+                    "title": "拉伸和width同时存在",
+                    "link": "all/demo-css/position/absolute-13.html"
+                }, {
+                    "title": "布局",
+                    "link": "all/demo-css/position/absolute-14.html"
+                }]
+            },
+            {
+                "title": "z-index",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "z-index",
+                    "link": "all/demo-css/z-index/z-index.html"
+                }]
+            },
+            {
+                "title": "flex",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "flex",
+                    "link": "all/demo-css/display/flex.html"
+                }]
+            },
+            {
+                "title": "float",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "float-文字环绕效果",
+                    "link": "all/demo-css/float/float-1.html"
+                }, {
+                    "title": "float-单侧固定自适应",
+                    "link": "all/demo-css/float/float-2.html"
+                }, {
+                    "title": "float-浮动与两侧皆自适应的流体布局",
+                    "link": "all/demo-css/float/float-3.html"
+                }, ]
+            },
+            {
+                "title": "overflow",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "overflow-基本特征、获取滚动栏width",
+                    "link": "all/demo-css/overflow/overflow-1.html"
+                }, {
+                    "title": "overflow-水平居中跳动问题的修复",
+                    "link": "all/demo-css/overflow/overflow-2.html"
+                }, {
+                    "title": "overflow-失效妙用",
+                    "link": "all/demo-css/overflow/overflow-3.html"
+                }, {
+                    "title": "overflow-两栏自适应布局",
+                    "link": "all/demo-css/overflow/overflow-4.html"
+                }, {
+                    "title": "table-cell-两栏自适应布局",
+                    "link": "all/demo-css/overflow/overflow-5.html"
+                }, {
+                    "title": "overflow-锚点定位与overflow选项卡",
+                    "link": "all/demo-css/overflow/overflow-6.html"
+                }]
+            },
+            {
+                "title": "margin",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "margin-基本特征、获取滚动栏width",
+                    "link": "all/demo-css/margin/margin.html"
+                }, {
+                    "title": "margin与writing的垂直居中",
+                    "link": "all/demo-css/margin/margin与writing的垂直居中.html"
+                }, {
+                    "title": "margin与absolute的垂直居中",
+                    "link": "all/demo-css/margin/margin与absolute的垂直居中.html"
+                }, {
+                    "title": "margin负值下的两端对齐",
+                    "link": "all/demo-css/margin/margin负值下的两端对齐.html"
+                }, {
+                    "title": "margin负值下的等高布局",
+                    "link": "all/demo-css/margin/margin负值下的等高布局.html"
+                }]
+            },
+            {
+                "title": "padding",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "padding",
+                    "link": "all/demo-css/padding/padding.html"
+                }]
+            },
+            {
+                "title": "border",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "border",
+                    "link": "all/demo-css/border/border.html"
+                }, {
+                    "title": "border实现两栏等高布局",
+                    "link": "all/demo-css/border/border实现两栏等高布局.html"
+                }]
             }, {
-                "title": "absolute",
-                "href": "https://www.22.com/"
+                "title": "background",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "linear-gradient",
+                    "link": "all/demo-css/background/linear-gradient.html"
+                }]
+            },
+            {
+                "title": "line-height",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "图片垂直居中",
+                    "link": "all/demo-css/line-height/图片垂直居中.html"
+                }, {
+                    "title": "多行文本垂直居中",
+                    "link": "all/demo-css/line-height/多行文本垂直居中.html"
+                }]
+            },
+            {
+                "title": "vertical-align",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "vertical-align",
+                    "link": "all/demo-css/vertical-align/vertical-align.html"
+                }, {
+                    "title": "图片垂直居中",
+                    "link": "all/demo-css/vertical-align/图片垂直居中.html"
+                }, {
+                    "title": "图片和文字垂直居中对齐",
+                    "link": "all/demo-css/vertical-align/图片和文字垂直居中对齐.html"
+                }, {
+                    "title": "任意数目列表两端对齐",
+                    "link": "all/demo-css/vertical-align/任意数目列表两端对齐.html"
+                }]
+            },
+            {
+                "title": "filter",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "drop-shadow",
+                    "link": "all/demo-css/filter/drop-shadow.html"
+                }]
+            },
+            {
+                "title": "其他",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "视觉格式化模型",
+                    "link": "all/demo-css/other/VFM.html"
+                }, {
+                    "title": "块级格式化上下文",
+                    "link": "all/demo-css/other/BFC.html"
+                }]
+            }
+        ]
+    },
+    //ECMAScript5
+    {
+        "title": "ECMAScript5",
+        "link": "javascript:;",
+        "child": [{
+                "title": "字符串方法",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "字符串方法",
+                    "link": "all/es5/字符串方法/字符串方法.html"
+                }, {
+                    "title": "charCodeAt和fromCharCode加密应用",
+                    "link": "all/es5/字符串方法/charCodeAt和fromCharCode加密应用.html"
+                }, {
+                    "title": "indexOf和lastIndexOf",
+                    "link": "all/es5/字符串方法/indexOf和lastIndexOf.html"
+                }, {
+                    "title": "substring的应用",
+                    "link": "all/es5/字符串方法/substring的应用.html"
+                }]
+            },
+            {
+                "title": "正则表达式",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "正则表达式",
+                    "link": "all/es5/正则表达式/正则表达式.html"
+                }, {
+                    "title": "正则表达式-偷小说",
+                    "link": "all/es5/正则表达式/正则表达式-偷小说.html"
+                }, {
+                    "title": "getByClass",
+                    "link": "all/es5/正则表达式/getByClass.html"
+                }]
+            }
+        ]
+    },
+    //ECMAScript6
+    {
+        "title": "ECMAScript6",
+        "link": "javascript:;",
+        "child": [{
+                "title": "项目构建",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "01基础架构",
+                    "link": "all/es6/项目构建/01基础架构.html"
+                }, {
+                    "title": "02项目目录创建",
+                    "link": "all/es6/项目构建/02项目目录创建.html"
+                }]
+            },
+            {
+                "title": "新增特性",
+                "link": "javascript:;",
+                "child": [{
+                    "title": "变量声明",
+                    "link": "all/es6/变量声明.html"
+                }, {
+                    "title": "解构赋值",
+                    "link": "all/es6/解构赋值.html"
+                }, {
+                    "title": "正则扩展",
+                    "link": "all/es6/正则扩展.html"
+                }]
+            }
+        ]
+    },
+    //Dome
+    {
+        "title": "Dome",
+        "link": "javascript:;",
+        "child": [{
+            "title": "导航栏",
+            "link": "javascript:;",
+            "child": [{
+                "title": "导航栏-纯css展现二级菜单",
+                "link": "all/demo/navigation/nav-1-css/nav-1-css.html"
+            }, {
+                "title": "原生js展现二级菜单",
+                "link": "all/demo/navigation/nav-1-js/nav-1-js.html"
+            }, {
+                "title": "纯css3旋转下拉菜单",
+                "link": "all/demo/navigation/nav-2/nav-2.html"
+            }, {
+                "title": "侧导航栏-3D展现二级菜单",
+                "link": "all/demo/navigation/side-nav-1/side-nav-1.html"
+            }, {
+                "title": "侧导航栏-淡出展现二三级菜单",
+                "link": "all/demo/navigation/side-nav-2/side-nav-2.html"
             }]
         }, {
-            "title": "display",
-            "href": "javascript:;",
-            "children": [{
-                "title": "flex",
-                "href": "https://www.21.com/"
-            }, {
-                "title": "block",
-                "href": "https://www.22.com/"
+            "title": "切换效果",
+            "link": "javascript:;",
+            "child": [{
+                "title": "最简单的图片切换",
+                "link": "all/demo/switch/最简单的图片切换.html"
+            }]
+        }, {
+            "title": "其他",
+            "link": "javascript:;",
+            "child": [{
+                "title": "css3开门动画",
+                "link": "all/other/open-door/open-door.html"
             }]
         }]
     },
+];
 
-    {
-        "title": "CSS",
-        "href": "https://www.2.com/",
-        "children": [{
-            "title": "position",
-            "href": "javascript:;",
-            "children": [{
-                "title": "relative",
-                "href": "https://www.21.com/"
-            }, {
-                "title": "absolute",
-                "href": "https://www.22.com/"
-            }]
-        }, {
-            "title": "display",
-            "href": "javascript:;",
-            "children": [{
-                "title": "flex",
-                "href": "https://www.21.com/"
-            }, {
-                "title": "block",
-                "href": "https://www.22.com/"
-            }]
-        }]
-    },
-
-    // {
-    //     "title": "HTML",
-    //     "href": "https://www.1.com/"
-    // },
-
-    // {
-    //     "title": "HTML",
-    //     "href": "https://www.1.com/"
-    // },
-
-    // {
-    //     "title": "HTML",
-    //     "href": "https://www.1.com/"
-    // },
-
-    // {
-    //     "title": "HTML",
-    //     "href": "https://www.1.com/"
-    // },
-]
-
-function navDataFn() {
-    return navData;
+function navData() {
+    return data;
 }
